a381f06ff9de999b050c9327e494247573a82400
Matthew Jackoski <mjackosk@mjackosk-mac11.local>
2020-05-03 21:20:20 -0700

    added the php check for cookie in view comments and added html text to add and view comments

--- taco_feedback/comment_server/add_comment.php
+++ taco_feedback/comment_server/add_comment.php
@@ -4,10 +4,54 @@
 
         $input = base64_encode(htmlspecialchars($_POST['comment']));
 
-        file_put_contents("../comments.html", $input."\n");
+        $fp = fopen('../comments.html', 'a');//opens file in append mode  
+        fwrite($fp, "\n".$input);   
+        fclose($fp);  
 
         echo <<<HERE
-        Comments have been added.
+        <html>
+            <head>
+            <title>Feedback Server</title>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="stylesheet" href="assets/css/main.css" />
+        </head>
+        <body>
+
+            <!-- Header -->
+                <header id="header" class="alt">
+                    <div class="logo"><a href="index.php">TACOS <span>are the best</span></a></div>
+                </header>
+
+
+        
+
+            <!-- Main -->
+                <div id="main">
+
+                    <!-- Section -->
+                        <section class="wrapper style1">
+                            <div class="inner">
+                                <!-- 2 Columns -->
+                                    <div class="flex flex-2">
+                                        <div class="col col1">
+                                            <div class="image round fit">
+                                                <img src="images/feedback.png" alt="" /></a>
+                                            </div>
+                                        </div>
+                                        <div class="col col2">
+                                            <h3>Comments have been added. The chief thanks you for your feedback!</h3>
+
+                                        </div>
+                                        
+                                    </div>
+                            </div>
+                        </section>
+
+                
+        </body>
+        </html>
+        
         HERE;
         
     }
--- taco_feedback/comment_server/view_comments.php
+++ taco_feedback/comment_server/view_comments.php
@@ -5,13 +5,61 @@ if(isset($_COOKIE['admin_id']) && $_COOKIE['admin_id'] == "kCxpAJacgNoGZPEPsgcdA
 
   $entries = file("../comments.html");
 
+  $out_string="";
+
   if(count($entries) > 0)
   {
     foreach($entries as $entry)
     {
-      echo base64_decode($entry);
+      $out_string.= "<h5>&emsp;&emsp;&emsp;Anonymous says:<br>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;".base64_decode($entry)."</h5><br>";
     }
   }
+  echo <<< HERE
+  <html>
+  <head>
+    <title>Feedback Server</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+  </head>
+  <body>
+
+    <!-- Header -->
+      <header id="header" class="alt">
+                <div class="logo"><a href="index.php">TACOS <span>are the best</span></a></div>
+      </header>
+
+
+
+
+    <!-- Main -->
+      <div id="main">
+
+        <!-- Section -->
+          <section class="wrapper style1">
+            <div class="inner">
+              <!-- 2 Columns -->
+                <div class="flex flex-2">
+                  <div class="col col1">
+                    <div class="image round fit">
+                      <img src="images/feedback.png" alt="" /></a>
+                    </div>
+                  </div>
+                  <div class="col col2">
+                  <h4>Comments:</h4>
+                     
+                  <div id="content"> $out_string</div>
+
+                  </div>
+                                    
+                </div>
+            </div>
+          </section>
+
+      
+  </body>
+  </html>
+  HERE;
 
 
 }
