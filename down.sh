19514c69320e25cb691384c9c2aa0b0379e39d1b
Paul Salessi <paulsalessi@gmail.com>
2020-06-19 13:00:46 -0700

    Update up/down scripts

--- down.sh
+++ down.sh
@@ -24,3 +24,15 @@ docker-compose down
 cd ../../Crypto/ssh_key_challenge
 
 docker-compose down
+
+cd ../../CTF-101/web-101
+
+docker-compose down
+
+cd ../network-101
+
+docker-compose down
+
+cd ../vi
+
+docker-compose down
--- up.sh
+++ up.sh
@@ -24,3 +24,15 @@ docker-compose up -d
 cd ../../Crypto/ssh_key_challenge
 
 docker-compose up -d
+
+cd ../../CTF-101/web-101
+
+docker-compose up -d
+
+cd ../network-101
+
+docker-compose up -d
+
+cd ../vi
+
+docker-compose up -d
